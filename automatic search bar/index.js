0e049ff70b7147b267f8e08f2820f42d0723b3a1
Sowmya Emani <119512783+sowmya061@users.noreply.github.com>
2024-05-04 12:28:30 +0530

    Add files via upload

---
+++ automatic search bar/index.js
@@ -0,0 +1,22 @@
+const availableKeywords = ['HTML', 'CSS', 'Web design', 'Where to learn coding'];
+const resultsBox = document.querySelector(".box");
+const inputBox = document.querySelector("input");
+
+inputBox.onkeyup = function () {
+    let result = [];
+    let input = inputBox.value;
+    if (input.length) {
+        result = availableKeywords.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+}
+
+function display(result) {
+    const content = result.map((list) => {
+        return "<li>" + list + "</li>";
+    }).join(""); // Joining the array elements into a single string
+    resultsBox.innerHTML = "<ul>" + content + "</ul>";
+}
